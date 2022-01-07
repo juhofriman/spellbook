079ab6b0ac812b2f1cb4711c59d9a959a43f7bda
Juho Friman <juho.t.friman@gmail.com>
2022-01-07 19:21:41 +0200

    Introduced simple webpack build and run from dist

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+const mode = 'development'
+const outputPath = path.resolve(__dirname, 'dist')
+
+const mainConfig = {
+  mode,
+  target: 'electron-main',
+  entry: './src/index.js',
+  output: {
+    path: outputPath,
+    filename: 'index.js'
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'index.html' }
+      ],
+    }),
+  ],
+}
+
+const rendererConfig = {
+  mode,
+  target: 'electron-renderer',
+  entry: './src/preload.js',
+  output: {
+    path: outputPath,
+    filename: 'preload.js'
+  }
+}
+
+module.exports = [mainConfig, rendererConfig]
